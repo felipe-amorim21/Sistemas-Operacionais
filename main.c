c78d51cd8b7ebd963b9aff0c94afcc2d47a6e486
Felipe Amorim <felipe21fab@gmail.com>
2023-09-23 16:50:07 -0300

    Add Tanenbaum pseudocode

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+#define TRUE 1
+
 int main(){
 
-    
+    while(TRUE){
+        type_prompt();
+        read_command(command, parameters);
+
+        if(fork() != 0){
+            /*Codigo do processo pai.*/
+            waitpid(-1, &status, 0);
+        } else {
+            /* Codigo do processo filho*/
+            execve(command, paramenters, 0);
+        }
+    }
+
+
 
 
     return 0;
