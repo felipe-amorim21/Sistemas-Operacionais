2c66cf5cf556fb9609722eeb8924e25359ecaf7d
Felipe Amorim <felipe21fab@gmail.com>
2023-09-23 17:28:52 -0300

    Created type_prompt function

--- main.c
+++ main.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void type_prompt();
 
 #define TRUE 1
 
 int main(){
 
+    int status, res;
+
     while(TRUE){
         type_prompt();
-        read_command(command, parameters);
-
-        if(fork() != 0){
+        //read_command(command, parameters);
+        res = fork();
+        if(res != 0){
             /*Codigo do processo pai.*/
-            waitpid(-1, &status, 0);
+            //waitpid(-1, &status, 0);
         } else {
             /* Codigo do processo filho*/
-            execve(command, paramenters, 0);
+            //execve(command, paramenters, 0);
         }
     }
 
@@ -21,4 +27,13 @@ int main(){
 
 
     return 0;
+}
+
+void type_prompt(){
+    char currentDirectory[300];
+  if (getcwd(currentDirectory, sizeof(currentDirectory)) != NULL) {
+    printf("%s$ ", currentDirectory);
+  } else {
+    perror("getcwd() error");
+  }
 }
